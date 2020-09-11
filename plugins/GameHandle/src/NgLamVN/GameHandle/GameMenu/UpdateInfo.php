11e5dd0192d3b36460bcefcf6abd241116d6ff4a
EzLam <winbam1357@gmail.com>
2020-09-11 22:16:34 +0700

    forget to update UpdateInfo :v

--- plugins/GameHandle/src/NgLamVN/GameHandle/GameMenu/UpdateInfo.php
+++ plugins/GameHandle/src/NgLamVN/GameHandle/GameMenu/UpdateInfo.php
@@ -20,6 +20,7 @@ class UpdateInfo
         $form->addLabel("-Vote system");
         $form->addLabel("Fixes:");
         $form->addLabel("Fix lỗi không dùng được lệnh sell");
+        $form->addLabel("Fix lỗi câu cá đôi khi bị văng game");
         $form->addLabel("**Vui lòng liên hệ admin nếu thấy giá không phù hợp hoặc thêm item bla bla :3");
         $form->addLabel("LƯU Ý:");
         $form->addLabel("• Vì server đang ở BETA (Thử nghiệm), nên chắc chắn rồi, nó không phải là 1 server hoàn thiện, do đó còn tồn tại rất nhiều lỗi, 1 số thứ không hoạt động.
