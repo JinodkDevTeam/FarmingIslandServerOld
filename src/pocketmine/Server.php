db8bf22c9604c4a527c246f7cfdb47be63e9ee7c
EzLam <winbam1357@gmail.com>
2020-10-15 10:23:52 +0700

    Update pmmp core.

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1960,6 +1960,9 @@ class Server{
 			$this->getNetwork()->blockAddress($entry->getName(), -1);
 		}
 
+		if($this->folderPluginLoader){
+			$this->pluginManager->registerInterface(new FolderPluginLoader($this->autoloader));
+		}
 		$this->pluginManager->registerInterface(new PharPluginLoader($this->autoloader));
 		$this->pluginManager->registerInterface(new ScriptPluginLoader());
         $this->pluginManager->loadPlugins($this->pluginPath);
