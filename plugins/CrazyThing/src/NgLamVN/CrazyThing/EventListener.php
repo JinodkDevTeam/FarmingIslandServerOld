dec491b3a98e6e66781f7ba2cab22f3c129f886d
EzLam <winbam1357@gmail.com>
2021-03-09 21:57:45 +0700

    Delete vendor, update crazything feature...

---
+++ plugins/CrazyThing/src/NgLamVN/CrazyThing/EventListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NgLamVN\CrazyThing;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class EventListener implements Listener
+{
+    public function __construct()
+    {
+    }
+
+    public function onJoin (PlayerPreLoginEvent $event)
+    {
+
+    }
+}
