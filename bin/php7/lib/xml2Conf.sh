9f5ebf9c8eb8724a8760572328834aa3040e7080
JINODK <jinodk2003@gmail.com>
2021-04-27 17:35:24 +0700

    Updated PHP to version 7.4 and add Chunkutil Updated PHP to 7.4 and add Chunkutil extension to increase server performance on this frickin' tiny server that cost basically nothing to run beside the cost of purchasing the board itself and a little bit of electricity + internet connection for it to connect to the outer world called "The Internet"

---
+++ bin/php7/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/maintainer/test/php-build-scripts/bin/php7/lib"
+XML2_LIBS="-lxml2 -L/home/maintainer/test/php-build-scripts/bin/php7/lib -lz     -lm "
+XML2_INCLUDEDIR="-I/home/maintainer/test/php-build-scripts/bin/php7/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
