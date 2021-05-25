830b93cfceb12eea37839829bd809c0f678cfc29
Ngọc Lam <nglamvn2911@gmail.com>
2021-05-25 10:08:51 +0700

    FlyCmd: Fix Bad logic...

--- plugins/GameHandle/src/NgLamVN/GameHandle/command/Fly.php
+++ plugins/GameHandle/src/NgLamVN/GameHandle/command/Fly.php
@@ -39,18 +39,18 @@ class Fly extends PluginCommand
                 $sender->sendMessage("Player not exist !");
                 return;
             }
-            if ($this->plugin->getPlayerStatManager()->getPlayerStat($sender)->isFly())
+            if (!$this->plugin->getPlayerStatManager()->getPlayerStat($player)->isFly())
             {
                 $player->setAllowFlight(true);
                 $player->setFlying(true);
-                $this->plugin->getPlayerStatManager()->getPlayerStat($sender)->setFly(true);
+                $this->plugin->getPlayerStatManager()->getPlayerStat($player)->setFly(true);
                 $sender->sendMessage($player->getName() . "  Enabled Fly");
             }
             else
             {
                 $player->setAllowFlight(false);
                 $player->setFlying(false);
-                $this->plugin->getPlayerStatManager()->getPlayerStat($sender)->setFly(false);
+                $this->plugin->getPlayerStatManager()->getPlayerStat($player)->setFly(false);
                 $sender->sendMessage($player->getName() . " Disabled Fly");
             }
             return;
@@ -60,16 +60,18 @@ class Fly extends PluginCommand
             $sender->sendMessage("You not have permission to use this command");
             return;
         }
-        if (!$sender->isFlying())
+        if (!$this->plugin->getPlayerStatManager()->getPlayerStat($sender)->isFly())
         {
             $sender->setAllowFlight(true);
             $sender->setFlying(true);
+            $this->plugin->getPlayerStatManager()->getPlayerStat($sender)->setFly(true);
             $sender->sendMessage("Enabled Fly");
         }
         else
         {
             $sender->setAllowFlight(false);
             $sender->setFlying(false);
+            $this->plugin->getPlayerStatManager()->getPlayerStat($sender)->setFly(false);
             $sender->sendMessage("Disabled Fly");
         }
     }
